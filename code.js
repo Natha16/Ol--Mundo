e2567c1eb71b94cc4af3c9ac4565c00f8e1a416a
Natha16 <161873686+Natha16@users.noreply.github.com>
2024-03-16 11:51:47 -0300

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const texto =document.body.querySelector("#texto");
   texto.addEventListener("click", () => {
     if (texto.textContent==="Olá Mundo")
-      texto.textContent="Hola MUndo";
+      texto.textContent="Hola Mundo";
     else
       texto.textContent="Olá Mundo";
   });
